a1d2127aabed360818a081e7ef66675d8da07b20
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-03-13 09:19:46 +0200

    failo generavimo klaidu testas

--- 0.1_versija/0.1_versija/generator_failai.cpp
+++ 0.1_versija/0.1_versija/generator_failai.cpp
@@ -11,3 +11,12 @@ int generuoti_pazymi() {
     static std::uniform_int_distribution<> dis(1, 10);
     return dis(gen);
 }
+
+void generuoti_faila(const std::string& filename, int studentu_skaicius, int nd_skaicius = 5) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::ofstream out(filename);
+    if (!out.is_open()) {
+        std::cerr << "Nepavyko sukurti failo: " << filename << "\n";
+        return;
+    }
