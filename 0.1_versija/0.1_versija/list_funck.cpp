dbf4c5edb29bc41244a06668ab0d4aa79f8e33e5
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-04-23 22:31:09 +0300

    skaitymo funkcija

--- 0.1_versija/0.1_versija/list_funck.cpp
+++ 0.1_versija/0.1_versija/list_funck.cpp
@@ -27,3 +27,40 @@ double mediana_l(const std::vector<int>& paz) {
 	size_t n = tmp.size();
 	return (n % 2 == 0) ? (tmp[n/2-1] + tmp[n/2]) / 2.0 : tmp[n/2];
 }
+
+void pasirinkimo_metodas_l(int tipas, std::list<StudentasL>& studentai) {
+	for (auto& s : studentai) {
+		double x = (tipas == 1) ? vidurkis_l(s.paz) : mediana_l(s.paz);
+		s.rez = galutinis_vid(x, s.egz);
+	}
+}
+
+void skaitymas_is_failo_l(const std::string& filename, std::list<StudentasL>& studentai) {
+	std::ifstream
+		file(filename);
+	if (!file.is_open())
+		throw std::runtime_error("Nepavyko atidaryti failo: " + filename);
+	
+	studentai.clear();
+	std::string header, line;
+	std::getline(file, header);
+	
+	int nr = 0;
+	while (std::getline(file, line)) {
+		++nr;
+		std::stringstream ss(line);
+		StudentasL s;
+		ss >> s.vardas >> s.pavarde;
+		int v;
+		while (ss >> v) s.paz.push_back(v);
+		if (s.paz.empty()) continue;
+		s.egz = s.paz.back();
+		s.paz.pop_back();
+		studentai.push_back(std::move(s));
+		if (nr % 100000 == 0)
+			std::cout << "  Nuskaityta: " << nr << " studentu...\n";
+	}
+	std::cout << "  Nuskaityta: " << nr << " studentu is failo: " << filename << "\n";
+}
+
+
