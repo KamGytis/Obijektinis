5acaf9025ff93b5db0411542186d051d8ec35a1f
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-04-23 22:16:19 +0300

    List.h pildymas

--- 0.1_versija/0.1_versija/list.h
+++ 0.1_versija/0.1_versija/list.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef LIST.h
+#define LIST_H	
+
+#include <string>
+#include <vector>	
+#include <list>
+
+struct StudentasL {
+	std::string vardas  = "";
+	std::string pavarde = "";
+	std::vector<int> paz;
+	int    egz = 0;
+	double rez = 0.00;
+};
+
+void ivesti_pazymius_l(StudentasL& s, int paz);
+double vidurkis_l(const std::vector<int>& paz);
+double mediana_l(const std::vector<int>& paz);
+
+
+#endif // !list.h
