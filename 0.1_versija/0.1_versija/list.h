a3c93727e0f79c1290162d29ca844e282bf0376a
Gytis Kaminskas <gytisskaminskas@gmail.com>
2026-04-24 14:10:38 +0300

    Update list.h

--- 0.1_versija/0.1_versija/list.h
+++ 0.1_versija/0.1_versija/list.h
@@ -26,6 +26,19 @@ double skirstymas_i_grupes_l(const std::list<StudentasL>& visi,
     std::list<StudentasL>& kieti,
     std::list<StudentasL>& vargsai);
 
+// STRATEGIJA 1 du nauji konteineriai, originalas lieka nepakeistas
+double skirstymas_s1_l(const std::list<StudentasL>& visi,
+    std::list<StudentasL>& kieti,
+	std::list<StudentasL>& vargsai);
+
+// STRATEGIJA 2 vienas naujas konteineris + erase/remove if
+double skirstymas_s2_l(std::list<StudentasL>& studentai,
+    std::list<StudentasL>& vargsai);
+
+// STRATEGIJA 3 splice - nulines kopijos
+double skirstymas_s3_l(std::list<StudentasL>& studentai,
+	std::list<StudentasL>& vargsai);
+
 void isvedimas_i_faila_l(const std::list<StudentasL>& studentai,
     const std::string& filename,
     const std::string& kategorija);
